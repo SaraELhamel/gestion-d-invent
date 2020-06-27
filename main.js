8ed135fee19571df24e5ea7f0d4a178e20c35ecc
SaraELhamel <saraelhamel88@gmail.com>
2020-06-27 21:31:04 +0100

    hh

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
   
  app.set('view engine', 'ejs');
   app.get('/', function(req, res) {
-    res.render('add');
+    res.render('home');
 });
 
 
@@ -28,54 +28,97 @@ var mysql      = require('mysql');
           console.log('Connected :D');
      }
  });
- app.post('/', urlencodedParser, function(req, res) {
-  const produit = {}
+//  app.post('/', urlencodedParser, function(req, res) {
+//   const produit = {}
   
-  produit.id = req.body.id 
-produit.fournisseur = req.body.fournisseur 
-  produit.produit = req.body.produit
-  produit.numrayon = req.body.numrayon
+//   produit.id = req.body.id 
+// produit.fournisseur = req.body.fournisseur 
+//   produit.produit = req.body.produit
+//   produit.numrayon = req.body.numrayon
  
 
-  connection.query('INSERT INTO produit SET ?', produit, (err, res) => {
-     if(err) throw err;
+//   connection.query('INSERT INTO produit SET ?', produit, (err, res) => {
+//      if(err) throw err;
 
 
-  // });
-  res.render('add');
+//   });
+//   res.render('add');
 
-});
- 
- 
- connection.query('SELECT * FROM produit', (err,result,fields) => {
-   if(err) throw err;
-  console.log('Data received from mia produit:');
-  console.log(result);
- });
-  const rayon = { id: '81', produit: 'pantouffles',fournisseur:'Ro',numrayon:'03' };
-  connection.query('INSERT INTO rayon SET ?', rayon, (err, res) => {
-    if(err) throw err;
-   console.log('Last insert ID:', res.insertId);
-  });
-  const produit = { id: '99', produit: 'serviette',fournisseur:'irla',numeroderayon:'10' };
-   connection.query('INSERT INTO produit SET ?', produit, (err, res) => {
-    if(err) throw err;
-    console.log('Last insert ID:', res.insertId);
+// });
+
+app.post('/update', urlencodedParser, (req, res) => {
+
+  let reqbody1 = [req.body.produit, req.body.fournisseur, req.body.numrayon, req.body.id]
+  let sql = 'UPDATE `produit` SET `produit`=?,`fournisseur`=?,`numrayon`=? where `id`=?'
+
+  connection.query(sql, reqbody1 ,  (error)  =>  {
+     if (error) {
+         console.log("NO CHANGE DATA")
+     } else {
+         console.log("Data have been changed")
+     }
+
    });
-  connection.query(
-   'UPDATE rayon SET produit = ? Where ID = ?',
-   ['jupe', 31],
-   (err, result) => {
-    if (err) throw err;
-     console.log(`Changed ${result.changedRows} row(s)`);
-   }
- );
+   res.redirect('/')
+});
+
+
+
+
+// app.post('/', urlencodedParser, function(req, res) {
+//   const produit = {}
+  
+//   produit.id = req.body.id 
+// produit.fournisseur = req.body.fournisseur 
+//   produit.produit = req.body.produit
+//   produit.numrayon = req.body.numrayon
+ 
+
+
+
+//   res.render('add');
+
+// });
+ 
+ 
+//  connection.query('SELECT * FROM produit', (err,result,fields) => {
+//    if(err) throw err;
+//   console.log('Data received from mia produit:');
+//   console.log(result);
+//  });
+//   const rayon = { id: '81', produit: 'pantouffles',fournisseur:'Ro',numrayon:'03' };
+//   connection.query('INSERT INTO rayon SET ?', rayon, (err, res) => {
+//     if(err) throw err;
+//    console.log('Last insert ID:', res.insertId);
+//   });
+//   const produit = { id: '99', produit: 'serviette',fournisseur:'irla',numrayon:'10' };
+//    connection.query('INSERT INTO produit SET ?', produit, (err, res) => {
+//     if(err) throw err;
+//     console.log('Last insert ID:', res.insertId);
+//    });
+//   connection.query(
+//    'UPDATE rayon SET produit = ? Where ID = ?',
+//    ['jupe', 31],
+//    (err, result) => {
+//     if (err) throw err;
+//      console.log(`Changed ${result.changedRows} row(s)`);
+//    }
+//  );
+app.get("/produit", (req, res) => {
+  connection.query('SELECT * FROM produit', (err, rows) => {
+      if (err) {
+          console.log("Error getting data")
+      } else {
+          res.render('home')
+      }
+  })
+});
  connection.query(
-   'DELETE FROM produit WHERE id = ?', [99], (err, result) => {
+   'DELETE FROM produit WHERE id = ?', [3], (err, result) => {
      if (err) throw err;
      console.log(`Deleted ${result.affectedRows} row(s)`);
-   }
- );
+   });
+ 
 
 app.listen(process.env.port || 3000,function(){
     console.log('now listening for request');
